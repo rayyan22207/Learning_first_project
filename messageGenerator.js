bfa1f93a0fb765f9f8fd36ae614bbf0202d5b282
rayyan22207 <rayyan22207@gmail.com>
2024-12-29 15:33:56 +0500

    working

--- messageGenerator.js
+++ messageGenerator.js
@@ -1,1 +1,27 @@
-console.log('test')
+const setups = [
+    "Why don't scientists trust atoms?",
+    "What do you call fake spaghetti?",
+    "Why couldn't the bicycle stand up by itself?",
+    "Why did the scarecrow win an award?",
+    "Why don't skeletons fight each other?"
+  ];
+  
+const punchlines = [
+    "Because they make up everything!",
+    "An impasta!",
+    "It was two-tired.",
+    "Because he was outstanding in his field.",
+    "They don't have the guts."
+  ];
+
+  
+const getRandomElement = arr => arr[Math.floor(Math.random() * arr.length)];
+
+const generateJoke = () => {
+    const setup = getRandomElement(setups);
+    const punchline = getRandomElement(punchlines);
+    return `${setup}\n${punchline}`;
+  };
+  
+  console.log(generateJoke());
+  
